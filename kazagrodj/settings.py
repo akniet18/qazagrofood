3ce58c04d62674b589950aa66e5d509a12489f5e
akniet18 <akniet.akniet2016@mail.ru>
2020-07-11 14:24:42 +0600

    first

--- kazagrodj/settings.py
+++ kazagrodj/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
