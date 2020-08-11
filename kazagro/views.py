233098e04c6c8787bc70b620624eab1828e7fa05
akniet18 <akniet.akniet2016@mail.ru>
2020-08-11 21:26:43 +0600

    first

--- kazagro/views.py
+++ kazagro/views.py
@@ -11,7 +11,6 @@ import gspread
 
 
 SAMPLE_SPREADSHEET_ID = '10W1gWd0Ftzb6iXuW1o9b1U4kLo_bpB9Em6V6oiSOweo'
-# SAMPLE_RANGE_NAME = 'Sheet1!A1:Ac'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 gc = gspread.service_account(filename=os.path.join(BASE_DIR, 'kazagrofood-dfd69b4ebbfd.json'))
 sh = gc.open_by_key(SAMPLE_SPREADSHEET_ID)
